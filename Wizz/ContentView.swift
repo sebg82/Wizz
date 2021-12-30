4b359d4bdd8d96255079cb70b9cf57a6a969e1ba
Sebastien Gohier <sebg82@hotmail.com>
2021-12-30 10:59:20 +0100

    First commit

---
+++ Wizz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wizz
+//
+//  Created by Sebastien Gohier on 30/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wizz/WizzApp.swift
@@ -0,0 +1,17 @@
+//
+//  WizzApp.swift
+//  Wizz
+//
+//  Created by Sebastien Gohier on 30/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct WizzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
